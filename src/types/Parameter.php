c760917d3a5db6c55630066ff36b7d6e4455d2b5
Πέτρος Ιωαννίδης <drweb3@ya.ru>
2025-12-28 14:33:01 +0300

    изменен тип параметра в описании на boolean иначе не проходит валидацию схемы

--- src/types/Parameter.php
+++ src/types/Parameter.php
@@ -7,7 +7,7 @@ class Parameter {
     const TYPE_INT = 'integer';
     const TYPE_STRING = 'string';
     const TYPE_FLOAT = 'float';
-    const TYPE_BOOL = 'bool';
+    const TYPE_BOOL = 'boolean';
     
     protected string $type;
     protected string $name;
